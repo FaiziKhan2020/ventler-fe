67165bd9725b02098e8bd3e2e213acd3439e8417
faizikhan1212 <mfk.khan2020@gmail.com>
2023-07-10 19:32:29 +0500

    Only Signin

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -129,18 +129,6 @@ const SignIn = ({ SignIn, showAlert, message }) => {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='/auth/forget' variant='body2'>
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/auth/signUp' variant='body2'>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
         <Typography
