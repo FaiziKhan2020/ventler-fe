b06a917ce3cdfb5e658e0688010b1a088a38d664
FaiziKhan2020 <mfk.khan2020@gmail.com>
2023-07-17 18:03:28 +0500

    table and cards added

--- src/pages/ChatBoardPage/panel/index.jsx
+++ src/pages/ChatBoardPage/panel/index.jsx
@@ -8,82 +8,156 @@ import {
   Divider,
   Stack,
   TextField,
-  Typography
-} from '@mui/material';
-import {useQuery} from 'react-query'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { getBaseApi } from '../../../config/Enviroment';
+  Typography,
+  Grid,
+} from "@mui/material";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { getBaseApi } from "../../../config/Enviroment";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import LinearProgress from "@mui/material/LinearProgress";
 const HtmlToReact = require("html-to-react").Parser;
 
-const generateArticle = async (url) =>{
-  return (await axios.post(getBaseApi()+"generate",{
-    url
-  }))
-}
+const generateArticle = async (url) => {
+  return await axios.post(getBaseApi() + "generate", {
+    url,
+  });
+};
 
-const Panel = () =>{
+const Panel = () => {
   const [url, setUrl] = useState("");
   const [loading, setLoading] = useState(false);
-  const [content, setContent] = useState(null)
+  const [content, setContent] = useState(null);
   const htmlToReactParser = new HtmlToReact();
+  const [age, setAge] = useState("");
 
-  function go(){
-    if(url){
-      setLoading(true)
-      generateArticle(url).then((data)=>{
-        setLoading(false)
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  function go() {
+    if (url) {
+      setLoading(true);
+      generateArticle(url).then((data) => {
+        setLoading(false);
         setContent(data.data.content);
-      })
+      });
     }
   }
-  function handleSubmit(){
-  }
-    return(
-      <main className='ml-0 h-full p-6 flex-grow'>
+  function handleSubmit() {}
+  return (
+    <main className="ml-0 h-full p-6 flex-grow">
       <form onSubmit={handleSubmit}>
-      <Card>
-        <CardHeader
-          subheader="Fetch & Regenrate Article"
-          title="Generate Content"
-        />
-        <CardContent>
-          <Stack
-            spacing={3}
-          >
-            <Box>
-            <Typography>Article URL</Typography>
-            <TextField
-              fullWidth
-              placeholder='https://newswebsite'
-              name="article"
-              type="text"
-              value={url}
-              onChange={(eve)=>setUrl(eve.target.value)}
-            />
-            </Box>
-
-          </Stack>
-        </CardContent>
-        <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button disabled={loading} onClick={go} variant="contained">
-            {loading ? "Generating Article": "Do The Magic"}
-          </Button>
-        </CardActions>
-      </Card>
-    </form>
-    {content &&
-    <Card>
-        <CardHeader
-          title="AI Generated Blog"
-        />
-        <CardContent style={{overflowY: 'scroll', maxHeight:'550px'}}>
-          {htmlToReactParser.parse(content)}
+        <Card>
+          <CardHeader
+            subheader="Fetch & Regenrate Article"
+            title="Generate Content"
+          />
+          <CardContent>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={10}>
+                <Typography>Article URL</Typography>
+                <TextField
+                  fullWidth
+                  placeholder="https://newswebsite"
+                  name="article"
+                  type="text"
+                  value={url}
+                  onChange={(eve) => setUrl(eve.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12} md={2} style={{ paddingTop: "40px" }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Wordpress Website
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Wordpress Website"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={10}>URL1</MenuItem>
+                    <MenuItem value={20}>URL2</MenuItem>
+                    <MenuItem value={30}>URL3</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
           </CardContent>
-    </Card>
-    }
-    </main> 
-    )
-}
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button disabled={loading} onClick={go} variant="contained">
+              {loading ? "Generating Article" : "Do The Magic"}
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
+      {/* <Typography variant="h5" style={{ paddingLeft: "20px" }}>
+        All Article Queue
+      </Typography> */}
+
+      {/* {content && ( */}
+      <Card>
+        <CardHeader title="All Article Queue" />
+        <CardContent
+          style={{
+            overflow: "scroll",
+            maxHeight: "400px",
+            overflowX: "hidden",
+          }}
+        >
+          <Grid container spacing={2}>
+            <Grid item md={6}>
+              <Card sx={{ minWidth: 220 }} style={{ background: "#f4f4f4" }}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 17, fontWeight: "bold", color: "black" }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </Typography>
+                  <Typography sx={{ fontSize: 14,mb:1 }} component="div">
+                    URL:
+                    <a
+                      style={{ color: "blue" }}
+                      href="
+                    https://www.google.com/search?q=dumpy+urls&oq=dumpy+urls&aqs=chrome..69i57j69i59l3.2404j0j7&sourceid=chrome&ie=UTF-8
+                    "
+                    >
+                      https://www.google.com/search?q=dumpy+urls&oq=dumpy+urls&aqs=chrome.
+                    </a>
+                  </Typography>
+
+                  <Typography variant="div">
+                    <FiberManualRecordIcon /> In Process
+                  </Typography>
+                  <LinearProgress sx={{ mt: 2 }} />
+                </CardContent>
+                <CardActions sx={{ mb: 2 }} style={{ float: "right" }}>
+                  <Button variant="outlined" size="small">
+                    Review Result
+                  </Button>
+                  <Button variant="contained" size="small">
+                    Regenerate Article
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+          {/* {htmlToReactParser.parse(content)} */}
+        </CardContent>
+      </Card>
+      {/* )} */}
+    </main>
+  );
+};
 
 export default Panel;
--- src/pages/ChatBoardPage/settings/index.jsx
+++ src/pages/ChatBoardPage/settings/index.jsx
@@ -1,88 +1,221 @@
-import { Label } from '@material-ui/icons';
+import Modal from "@mui/material/Modal";
+import { styled } from "@mui/material/styles";
 import {
-    Box,
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    CardHeader,
-    Divider,
-    Stack,
-    TextField,
-    Typography
-  } from '@mui/material';
-import { useCallback, useState } from 'react';
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Stack,
+  TextField,
+  Typography,
+  Grid,
+} from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useCallback, useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "800px",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
 
 const Settings = () => {
-    const [values, setValues] = useState({
-        wordpress: '',
-        openai: ''
-      });
+  const [values, setValues] = useState({
+    wordpress: "",
+    openai: "",
+  });
+  const [open, setOpen] = useState(false);
 
-    const handleChange = useCallback(
-        (event) => {
-          setValues((prevState) => ({
-            ...prevState,
-            [event.target.name]: event.target.value
-          }));
-        },
-        []
-      );
-    
-      const handleSubmit = useCallback(
-        (event) => {
-          event.preventDefault();
-        },
-        []
-      );
+  const handleChange = useCallback((event) => {
+    setValues((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  }, []);
 
-    return(
-        <main className='ml-0 h-full p-6 flex-grow'>
-    <form onSubmit={handleSubmit}>
+  const handleSubmit = useCallback((event) => {
+    event.preventDefault();
+  }, []);
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  function createData(name, calories, fat) {
+    return { name, calories, fat };
+  }
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const rows = [
+    createData(
+      "My Korean Website",
+      "https://koreansite.com",
+      "hereAddKey_65464654564"
+    ),
+    createData(
+      "My German Website",
+      "https://Germansite.com",
+      "hereAddKey_65464654564"
+    ),
+    createData(
+      "My Blogging Website",
+      "https://Bloggingsite.com",
+      "hereAddKey_65464654564"
+    ),
+  ];
+
+  return (
+    <main className="ml-0 h-full p-6 flex-grow">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="child-modal-title"
+        aria-describedby="child-modal-description"
+      >
+        <Box sx={{ ...style }}>
+          <Typography variant="h5" sx={{ mb: 2 }}>
+            Add Credentials
+          </Typography>
+          <Typography>Wordpress Title</Typography>
+          <TextField
+            fullWidth
+            placeholder="blogging"
+            name="article"
+            type="text"
+          />
+          <Typography sx={{ mt: 2 }}>URL</Typography>
+          <TextField
+            fullWidth
+            placeholder="example@web.com"
+            name="article"
+            type="text"
+          />
+          <Typography sx={{ mt: 2 }}>Credentials</Typography>
+          <TextField
+            fullWidth
+            placeholder="Wp_4654564646"
+            name="article"
+            type="text"
+          />
+
+          <Button
+            variant="contained"
+            sx={{ mt: 2 }}
+            style={{ float: "right" }}
+            onClick={handleClose}
+          >
+            ADD
+          </Button>
+        </Box>
+      </Modal>
+      <form onSubmit={handleSubmit}>
+        <Card>
+          <CardHeader subheader="Setup Keys & Credentials" title="Settings" />
+          <CardContent>
+            <Stack spacing={3}>
+              <Box>
+                <Typography>OpenAI Key</Typography>
+                <TextField
+                  fullWidth
+                  placeholder="OpenAI Secret Key"
+                  name="openai"
+                  onChange={handleChange}
+                  type="text"
+                  value={values.openai}
+                />
+              </Box>
+            </Stack>
+          </CardContent>
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button variant="contained">Update</Button>
+          </CardActions>
+        </Card>
+      </form>
+
       <Card>
         <CardHeader
-          subheader="Setup Keys & Credentials"
-          title="Settings"
+          subheader="Configure Wordpress Sites"
+          title="Wordpress Sites"
         />
-        <CardContent>
-          <Stack
-            spacing={3}
-          >
-            <Box>
-            <Typography>Wordpress Credentials</Typography>
-            <TextField
-              fullWidth
-              placeholder='Wordpress Secret Credentials'
-              name="wordpress"
-              onChange={handleChange}
-              type="text"
-              value={values.wordpress}
-            />
-            </Box>
-
-            <Box>
-            <Typography>OpenAI Key</Typography>
-            <TextField
-              fullWidth
-              placeholder='OpenAI Secret Key'
-              name="openai"
-              onChange={handleChange}
-              type="text"
-              value={values.openai}
-            />
-            </Box>
-
-          </Stack>
-        </CardContent>
-        <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">
-            Update
-          </Button>
-        </CardActions>
       </Card>
-    </form>
-        </main>
-    )
-}
+
+      <Box style={{ float: "right", marginBottom: "10px" }}>
+        <Button onClick={handleOpen} variant="contained">
+          add
+        </Button>
+      </Box>
+
+      <Grid container>
+        <TableContainer
+          component={Paper}
+          style={{
+            overflow: "scroll",
+            maxHeight: "300px",
+            overflowX: "hidden",
+          }}
+        >
+          <Table aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>Website Name</StyledTableCell>
+                <StyledTableCell>URL</StyledTableCell>
+                <StyledTableCell>Credentials</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell>{row.calories}</StyledTableCell>
+                  <StyledTableCell>{row.fat}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </main>
+  );
+};
 
 export default Settings;
