85d909f6b114c074e2f022b70a664ce6d65a192e
FaiziKhan2020 <mfk.khan2020@gmail.com>
2023-08-03 16:49:31 +0500

    fix

--- src/config/Enviroment.js
+++ src/config/Enviroment.js
@@ -1,6 +1,6 @@
 // import dotenv from "dotenv";
  export const getBaseApi = () => {
-   return process.env.REACT_APP_BASE_API || "https://ventler-br.onrender.com/";
+   return process.env.REACT_APP_BASE_API || "https://ventler-be.onrender.com/";
  };
 // export const getBaseApi = () => {
 //  return process.env.REACT_APP_BASE_API || "http://localhost:8000/";
